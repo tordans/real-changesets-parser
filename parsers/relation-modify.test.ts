1ca05e5aec10ae62985314931cdf59326509ff92
Tobias <tobias@fixmycity.de>
2024-06-10 06:03:19 +0200

    Cleanup missplaced file

--- parsers/relation-modify.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from 'vitest'
-import { relationModifyInput } from '../tests/data/relation-modify-input'
-import { relationModifyOutput } from '../tests/data/relation-modify-output'
-import { realChangesetElementParser } from './realChangesetElementParser'
-
-test('handle relation modify case', () => {
-  const actualOutput = realChangesetElementParser(relationModifyInput)
-  expect(actualOutput).toEqual(relationModifyOutput)
-})
